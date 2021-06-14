d6b62a534ec9517f63329025d4a9b3db2e50ee84
kjcaway <whdckstngh@naver.com>
2021-06-14 22:38:57 +0900

    **ADD -checkbox practice

--- ui/main/contents_one.js
+++ ui/main/contents_one.js
@@ -19,6 +19,26 @@
         { "name": "Android9", "parentId": 2, "id": 27 },
         { "name": "Android10", "parentId": 2, "id": 28 },
         { "name": "Android11", "parentId": 2, "id": 29 },
+    ],
+    services: [
+        { "category": "Game", "name": "Starcraft", "parentId": 11, "id": 101 },
+        { "category": "Game", "name": "Warcraft", "parentId": 11, "id": 102 },
+        { "category": "Game", "name": "Counter Strike", "parentId": 11, "id": 103 },
+        { "category": "Game", "name": "Minecraft", "parentId": 12, "id": 104 },
+        { "category": "Game", "name": "FiFA", "parentId": 13, "id": 105 },
+        { "category": "Game", "name": "NBA 20K", "parentId": 13, "id": 106 },
+        { "category": "Game", "name": "CookieRun", "parentId": 21, "id": 107 },
+        { "category": "Game", "name": "COC", "parentId": 28, "id": 108 },
+        { "category": "Game", "name": "ClanOfWar", "parentId": 29, "id": 109 },
+        { "category": "Game", "name": "삼국지", "parentId": 29, "id": 110 },
+        { "category": "Game", "name": "프랑프랑", "parentId": 29, "id": 111 },
+        { "category": "Program", "name": "굿닥터", "parentId": 11, "id": 201 },
+        { "category": "Program", "name": "익스플로러", "parentId": 13, "id": 202 },
+        { "category": "Program", "name": "크롬", "parentId": 13, "id": 203 },
+        { "category": "Program", "name": "엣지", "parentId": 14, "id": 204 },
+        { "category": "Program", "name": "V3 Security", "parentId": 12, "id": 205 },
+        { "category": "Program", "name": "FireWall", "parentId": 28, "id": 206 },
+        { "category": "Program", "name": "NetDoctor", "parentId": 29, "id": 207 },
     ]
 }
 
@@ -39,12 +59,32 @@ var CONTENTS_ONE = {
         html = "";
         DATA_MAP.products.forEach(function (product) {
             html += `
-            <label class='tag form-check-label text-capitalize badge badge-success m-2' for='prd_${product.id}'>
-                <input data-group='${product.parentId}' id='prd_${product.id}' name='productsChk' type='checkbox' class='form-check-input'>${product.name}
+            <label class='tag form-check-label text-capitalize badge badge-secondary m-2' for='prd_${product.id}'>
+                <input data-group='${product.parentId}' id='prd_${product.id}' value='${product.id}' name='productsChk' type='checkbox' class='form-check-input'>${product.name}
             </label>
             `
         })
         $("#productDiv").html(html);
+
+
+       /* html = "";
+        let html2 = "";
+        DATA_MAP.services.forEach(function (service) {
+            if (service.category == 'Game') {
+                html += `
+                <label class='tag form-check-label text-capitalize badge badge-success m-2' for='prd_${product.id}'>
+                    <input data-group='${product.parentId}' id='prd_${product.id}' value='${product.id}' name='productsChk' type='checkbox' class='form-check-input'>${product.name}
+                </label>
+                `
+
+            } else {
+                html2 += `
+                
+                `
+            }
+        });
+        $("#serviceDiv").html(html+html2);*/
+
     },
     initEventHandler: function () {
         /** init event handler **/
@@ -61,6 +101,18 @@ var CONTENTS_ONE = {
 
                 CONTENTS_ONE.applyProductView(CONTENTS_ONE.data.selectedProductGroup);
             }
+        })
+
+        $('input[name=productsChk]').on("change", function () {
+            let arr = [];
+            $("input:checkbox[name=productsChk]:checked").each(function () {
+                arr.push($(this).val());
+            });
+            CONTENTS_ONE.data.selectedProducts = arr;
+
+            console.log(CONTENTS_ONE.data.selectedProducts)
+
+            $('#collapseTwo').collapse('show');
         })
     },
     applyProductGroupView: function (data) {
