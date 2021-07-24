3be65ecfd15f54b919ce30262e05824ebe3ed73b
kjcaway <whdckstngh@naver.com>
2021-07-24 15:07:58 +0900

    **MOD -convert exception response body to json

--- filters/ExceptionFilter.cs
+++ filters/ExceptionFilter.cs
@@ -1,7 +1,9 @@
 ﻿using DemoWebApi.Exceptions;
 using log4net;
+using Newtonsoft.Json;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
 using System.Web.Http.Filters;
 
@@ -10,18 +12,21 @@ namespace DemoWebApi.filters
     public class ExceptionFilter : ExceptionFilterAttribute
     {
         private readonly ILog logger = LogManager.GetLogger(typeof(ExceptionFilter));
-
+        
         public override void OnException(HttpActionExecutedContext context)
         {
             if(context.Exception is BaseException)
             {
                 HttpStatusCode httpCode = (context.Exception as BaseException).httpCode;
-                string message = (context.Exception as BaseException).message;
+                string jsonStr = JsonConvert.SerializeObject(new { 
+                    code = httpCode,
+                    message = (context.Exception as BaseException).message 
+                });
 
                 throw new HttpResponseException(new HttpResponseMessage(httpCode)
                 {
-                    Content = new StringContent(message),
-                    ReasonPhrase = message
+                    Content = new StringContent(jsonStr, Encoding.UTF8, "application/json"),
+                    ReasonPhrase = null
                 });
             }
             logger.Error(context.Exception);
