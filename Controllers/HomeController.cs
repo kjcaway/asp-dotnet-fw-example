c131c39ca9d9b9d55277be3012d029f95dfb8538
kjcaway <whdckstngh@naver.com>
2021-09-26 15:18:15 +0900

    **ADD -add test controller for testing socket hang up

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using DemoWebApi.Dto;
+using DemoWebApi.Exceptions;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+
+namespace DemoWebApi.Controllers
+{
+    public class HomeController: ApiController
+    {
+        readonly string version = WebConfigurationManager.AppSettings["VERSION"].ToString();
+
+        [Route("api/home/")]
+        [HttpGet]
+        public JsonResult<ApiResult> Index()
+        {
+            HttpContext context = HttpContext.Current;
+
+            context.AddError(new BaseException("New Exception #2"));
+
+            context.Response.Close();
+
+            return Json(new ApiResult
+            {
+                version = version,
+                result = "OK"
+            });
+        }
+    }
+}
