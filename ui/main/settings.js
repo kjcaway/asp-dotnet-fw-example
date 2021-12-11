4b8aa204167e75a330b73806361a88fc64df806f
kjcaway <whdckstngh@naver.com>
2021-12-11 16:23:36 +0900

    **ADD -add bootstrap treeview example -ref. treeview(develop branch)

--- ui/main/settings.js
+++ ui/main/settings.js
@@ -1,26 +1,99 @@
-﻿let MAIN = {
-    onAddCardClick: function (e) {
-        const jqCard = $(`
-            <div class="col-sm-3 mt-2">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">제목</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">서브제목</h6>
-                        <p class="card-text">블라블라</p>
-                        <a href="#" class="card-link">Red</a>
-                        <a href="#" class="card-link">Green</a>
-                    </div>
-                </div>
-            </div>
-        `);
+﻿
 
-        jqCard.hide();
-        $("#cardGroupDiv").append(jqCard);
-        jqCard.show("normal");
+let MAIN = {
+    init: function () {
+        $('#tree').treeview({
+            data: getTree(),
+            expandIcon: 'fa fa-chevron-down',
+            collapseIcon: 'fa fa-chevron-up'
+        });
+        
+        $('#tree').on('nodeSelected', function (event, data) {
+            console.log('nodeSelected ' + data.nodeId);
+            MAIN.showLeftSelectedNode(data);
+        });
+
+        $('#tree').on('nodeUnselected', function (event, data) {
+            console.log('nodeUnselected ' + data.nodeId);
+            MAIN.hideLeftSelectedNode(data);
+        });
+
+        $('#add-btn').on('click', function () {
+            let selected = $('#tree').treeview('getSelected');
+            console.log(selected);
+
+            if (selected.length > 0) {
+                $('#tree').treeview('addNode', [{
+                    text: "Added Node"
+                }, selected[0], 0, { silent: true }]);
+            }
+        });
+        $('#delete-btn').on('click', function () {
+            let selected = $('#tree').treeview('getSelected');
+            console.log(selected);
+
+            if (selected.length > 0) {
+                $('#tree').treeview('removeNode', [selected[0], { silent: true }]);
+            }
+        });
+    },
+    showLeftSelectedNode: function (data) {
+        $("#info-node").html("");
+        $("#info-node").html(`
+            <h5>${data.text}</h5>
+            <ui>
+                <li>id : ${data.nodeId}</li>
+                <li>desc : ${data.desc}</li>
+        `);
+    },
+    hideLeftSelectedNode: function (data) {
+        $("#info-node").html("");
     }
 };
 
+function getTree() {
+    var tree = [
+        {
+            text: "<span class='fa fa-home'></span>Parent 1",
+            nodeId: "dlewjfkljwf",
+            desc: "custom",
+            nodes: [
+                {
+                    text: "Child 1",
+                    nodes: [
+                        {
+                            text: "Grandchild 1"
+                        },
+                        {
+                            text: "Grandchild 2"
+                        }
+                    ]
+                },
+                {
+                    text: "Child 2"
+                }
+            ]
+        },
+        {
+            text: "Parent 2",
+            icon: "glyphicon glyphicon-stop",
+            selectedIcon: "glyphicon glyphicon-stop",
+            color: "#000000",
+            backColor: "#FFFFFF",
+            href: "#node-1",
+            selectable: true,
+            state: {
+                checked: true,
+                disabled: true,
+                expanded: true,
+                selected: false
+            },
+            tags: ['available'],
+        }
+    ];
 
+    return tree;
+}
 
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
@@ -29,5 +102,6 @@ $(document).ready(function () {
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
     
-    $("#addCardBtn").on("click", MAIN.onAddCardClick);
+   
+    MAIN.init();
 });
